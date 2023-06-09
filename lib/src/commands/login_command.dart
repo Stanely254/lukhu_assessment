af94b4ced674e9018911c5d7cd43883e24a2b6de
Starnorh <stanelymyuga.12@gmail.com>
2023-06-09 10:17:04 +0300

    removed  unused code

--- lib/src/commands/login_command.dart
+++ lib/src/commands/login_command.dart
@@ -12,10 +12,6 @@ class LoginCommand extends AbstractCommand {
 
   @override
   Future<void> execute() async {
-    if (!appController.isOnline) {
-      ShortMessage.showMessage(message: "Check your internet connection!");
-      return;
-    }
     var loginForm = authController.loginFormKey;
     debugPrint('[DATA] ${authController.loginData}');
     if (loginForm.currentState!.validate()) {
